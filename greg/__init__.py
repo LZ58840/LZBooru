ec7237ce4642dc8d01ebad35f7c193852eff862d
Lucas Zhou <lcszhdrw@gmail.com>
2021-12-21 11:03:55 -0500

    api functions for greg, part 1

--- greg/__init__.py
+++ greg/__init__.py
@@ -1,6 +1,6 @@
-from greg.api import get_subreddits
+from greg.api import get_booru_subreddits
 
 
 def GregDaemon(local_handler):
-    get_subreddits()
+    get_booru_subreddits()
     local_handler.enter(60, 1, GregDaemon, (local_handler,))
